becd055fb55e2591fae79233d078a56cbb3e9d60
NDAK GAMERS <devinip2000@gmail.com>
2024-07-23 22:46:03 +0700

    middleware and endpoint for login

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['email yang anda masukkan salah'],
+            ]);
+        }
+
+     
+        return response()->json([
+            'status' => true,
+            'message' => 'Login Berhasil',
+            'Token Anda' => $user->createToken('user login')->plainTextToken
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
@@ -11,17 +12,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/book', [BookController::class, 'index']);
-Route::post('/book', [BookController::class, 'store']);
-Route::get('/book/{id}', [BookController::class, 'show']);
-Route::post('/book/{id}', [BookController::class, 'update']);
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
+Route::get('/book', [BookController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/book', [BookController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/book/{id}', [BookController::class, 'show'])->middleware('auth:sanctum');
+Route::post('/book/{id}', [BookController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/book/{id}', [BookController::class, 'destroy'])->middleware('auth:sanctum');
 
-Route::get('/book-categories', [CategoryController::class, 'index']);
-Route::post('/book-categories', [CategoryController::class, 'store']);
-Route::get('/book-categories/{id}', [CategoryController::class, 'show']);
-Route::put('/book-categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/book-categories/{id}', [CategoryController::class, 'destroy']);
+Route::get('/book-categories', [CategoryController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/book-categories', [CategoryController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/book-categories/{id}', [CategoryController::class, 'show'])->middleware('auth:sanctum');
+Route::put('/book-categories/{id}', [CategoryController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/book-categories/{id}', [CategoryController::class, 'destroy'])->middleware('auth:sanctum');
 
-Route::patch('/book-categories/{id}/publish', [CategoryController::class, 'publish']);
-Route::patch('/book-categories/{id}/unpublish', [CategoryController::class, 'unpublish']);
+Route::patch('/book-categories/{id}/publish', [CategoryController::class, 'publish'])->middleware('auth:sanctum');
+Route::patch('/book-categories/{id}/unpublish', [CategoryController::class, 'unpublish'])->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
